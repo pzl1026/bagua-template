0d231b082d38d470091ee310e8766623c1616db9
pzl <pzl1026@aliyun.com>
2020-09-14 17:58:55 +0800

    tijiao

--- react/.bagua.js
+++ react/.bagua.js
@@ -4,7 +4,6 @@ module.exports = {
   name: 'react',
   isTop: false,
   shared: [{ react: { singleton: true } }],
-  library: { type: 'var', name: 'app2'},
   dev: {
     st1: {
       port: '3002',
